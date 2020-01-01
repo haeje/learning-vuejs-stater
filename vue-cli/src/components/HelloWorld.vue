2d75b1a27d7e1164a2e233277ca810263f935663
haja <parkhaeje@gmail.com>
2020-01-01 23:35:42 +0900

    vue learning data-binding, cli

---
+++ vue-cli/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <form action="" v-on:submit.prevent="submitEvent">
+    <div>
+      <label for="username">id : </label>
+      <input type="text" name="" id="username" v-model="username">
+    </div>
+    
+    <div>
+      <label for="password">password : </label>
+      <input type="text" name="" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data: function(){
+    return {
+      username : '',
+      password : '',
+    }
+  },
+  methods:{
+    submitEvent:function(){
+      // event.preventDefault();
+      const url = "https://jsonplaceholder.typicode.com/users";
+      const data = {
+        username : this.username,
+        password : this.password
+      }
+      console.log(data);
+      
+      axios.post(url, data)
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function(err){
+          console.log(err);
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
